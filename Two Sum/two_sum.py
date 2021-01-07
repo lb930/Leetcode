f95b7396e3cfd1b933133e953a67d3cf3150f0ae
lb930 <59416844+lb930@users.noreply.github.com>
2021-01-07 17:18:50 +0100

    Update two_sum.py

--- Two Sum/two_sum.py
+++ Two Sum/two_sum.py
@@ -4,17 +4,4 @@ def two_sum_dict(nums, target):
         comp = target - num
         if num in new_dict:
             return new_dict[num], i 
-        new_dict[comp] = i   
-
-print(two_sum_dict([3, 2, 4], 6))
-
-# Alternative
-def two_sum(nums, target):
-    new_list = []
-    for i in range(len(nums)):
-        num = target - nums[i]
-        if nums[i] in new_list:
-            return nums.index(num), i
-        new_list.append(num)
-
-print(two_sum([3, 2, 4], 6))
+        new_dict[comp] = i 
